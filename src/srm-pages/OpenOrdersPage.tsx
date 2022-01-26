a42e11db259b65b2f5bcd6787ec86e75758a0c88
Poliarenko Olena <p.olena@4irelabs.com>
2022-01-26 11:31:43 +0200

    BN-58: Replase of srm-*(assets, components, pages, utils, wallet-adapters)

---
+++ src/srm-pages/OpenOrdersPage.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import FloatingElement from '../srm-components/layout/FloatingElement';
+import {
+  useAllMarkets,
+  useAllOpenOrders,
+  useMarketInfos,
+} from '../srm-utils/markets';
+import OpenOrderTable from '../srm-components/UserInfoTable/OpenOrderTable';
+import { Button, Row } from 'antd';
+import { OrderWithMarketAndMarketName } from '../srm-utils/types';
+import { useWallet } from '../srm-utils/wallet';
+import WalletConnect from '../srm-components/WalletConnect';
+
+export default function OpenOrdersPage() {
+  const { connected } = useWallet();
+  const { openOrders, loaded, refreshOpenOrders } = useAllOpenOrders();
+  let marketInfos = useMarketInfos();
+  let marketAddressesToNames = Object.fromEntries(
+    marketInfos.map((info) => [info.address.toBase58(), info.name]),
+  );
+  let [allMarkets] = useAllMarkets();
+  const marketsByAddress = Object.fromEntries(
+    (allMarkets || []).map((marketInfo) => [
+      marketInfo.market.address.toBase58(),
+      marketInfo.market,
+    ]),
+  );
+
+  const dataSource: OrderWithMarketAndMarketName[] = (openOrders || [])
+    .map((orderInfos) =>
+      orderInfos.orders.map((order) => {
+        return {
+          marketName: marketAddressesToNames[orderInfos.marketAddress],
+          market: marketsByAddress[orderInfos.marketAddress],
+          ...order,
+        };
+      }),
+    )
+    .flat();
+
+  if (!connected) {
+    return (
+      <Row
+        justify="center"
+        style={{
+          marginTop: '10%',
+        }}
+      >
+        <WalletConnect />
+      </Row>
+    );
+  }
+
+  return (
+    <FloatingElement style={{ flex: 1, paddingTop: 10 }}>
+      <Button onClick={refreshOpenOrders} loading={!loaded}>
+        Refresh
+      </Button>
+      <OpenOrderTable
+        openOrders={dataSource}
+        pageSize={25}
+        loading={!loaded}
+        onCancelSuccess={refreshOpenOrders}
+        marketFilter
+      />
+    </FloatingElement>
+  );
+}

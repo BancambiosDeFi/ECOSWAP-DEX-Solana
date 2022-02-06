c38f82b9c36b3674700c81933d020dacc5a6d295
Poliarenko Olena <p.olena@4irelabs.com>
2022-02-06 16:29:29 +0200

    BN-4: footer component created, it contains - active links, block for placing social links and form. Add these elements to the design system page. Works on mobile and desktop device.

---
+++ src/srm-components/SwapFooter.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Layout, Row, Col } from 'antd';
+import Link from './Link';
+const { Footer } = Layout;
+
+const footerElements = [
+  { description: 'Blog', link: "#" },
+  { description: 'Twitter', link: "#" },
+  { description: 'Telegram', link: "#" },
+  { description: 'Discord', link: "#" },
+  { description: 'Instagram', link: "#" },
+  {description: 'LinkedIn', link: "#"},
+];
+
+export const SwapFooter = () => {
+  return (
+    <Footer
+      style={{
+        height: '59px',
+        background: "#1A1B1C",
+        display: 'flex',
+        alignItems:  'center',
+        width: "100%",
+      }}
+    >
+      <Row style={{
+        display: 'flex',
+        justifyContent:  "space-around",
+        width: '100%',
+      }}>
+            {footerElements.map((elem, index) => {
+              return (
+                <Col key={index + ''}>
+                  <Link style={{
+                    color: "#FFFFFF",
+                    textTransform: "none",
+                    fontFamily: '"Spy Agency", sans-serif',
+                    fontSize: '14px',
+                    fontStyle: 'normal',
+                  }} external to={elem.link}>
+                    {elem.description}
+                  </Link>
+                </Col>
+              );
+            })}
+      </Row>
+    </Footer>
+  );
+};

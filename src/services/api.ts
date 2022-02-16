e7bf25df145f07295ba4f9b02955b0e20c1cbb1b
ikhanenko <anymacstore@ikhanenko-mac.local>
2022-02-16 21:07:47 +0200

    Finished BN-94 and made price impact logic by raydium-sdk Liquidity.computeAmountOut func.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// export const API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical';
 export const API_URL = 'https://api.nomics.com/v1/exchange-rates/history';
 export const API_KEY = 'a0d947e2da0f40f0cf0b078c07100329c1e25369';
 
@@ -12,8 +11,9 @@ export const getQuotesHistorical = (tokenSymbol: string, start: string, end: str
       start,
       end,
     },
-    // headers: {
-    //   'X-CMC_PRO_API_KEY': '7b4b4f68-3f53-4287-a75e-c2cc99d4b930',
-    // },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    },
   });
 };

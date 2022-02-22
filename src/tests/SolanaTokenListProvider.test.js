54edab557c0a6b98f21d2cbeb4c98f9718e8fb41
chikini91 <khizhnyak.oleg91@gmail.com>
2022-02-22 17:31:07 +0200

    fix

--- src/tests/SolanaTokenListProvider.test.js
+++
@@ -1,9 +0,0 @@
-import { TokenListProvider } from '@solana/spl-token-registry';
-
-describe('TokenListProvider', () => {
-  it('should return array of objects', async () => {
-    const response = await new TokenListProvider().resolve();
-    const data = response.getList();
-    expect(data).toBeTruthy();
-  });
-});

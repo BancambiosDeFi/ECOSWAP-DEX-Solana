e4c2618505f7ef0461d99d31d1cc3f64033f0238
chikini91 <khizhnyak.oleg91@gmail.com>
2022-04-04 11:57:53 +0300

    fix

--- src/pages/utils.tsx
+++ src/pages/utils.tsx
@@ -1,6 +1,4 @@
 // https://www.cryptocompare.com/cryptopian/api-keys
-// login: bancambios@gmail.com
-// pass: B4nc4Mbi0$/22
 const chart_api_key = '797989272d5a7ebac9489340659636373fe5ee73bf237a215a8d1667fcdd7095';
 
 export { chart_api_key };

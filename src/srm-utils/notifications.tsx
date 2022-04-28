6d366c48168596c533ccb957d217721460747e6d
Dzmitry Panamarenka <d.panamarenka@4irelabs.com>
2022-04-29 00:16:43 +0200

    BN-218: add proper add liquidity notifications

--- src/srm-utils/notifications.tsx
+++ src/srm-utils/notifications.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import { notification } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
+import { makeStyles } from '@mui/styles';
 import BodyText from '../components/typography/BodyText';
 import H3Text from '../components/typography/H3Text';
-import SuccessIcon from '../srm-assets/check-circle.svg';
-import ErrorIcon from '../srm-assets/close-circle.svg';
+import { ReactComponent as SuccessIcon } from '../srm-assets/check-circle.svg';
+import { ReactComponent as ErrorIcon } from '../srm-assets/close-circle.svg';
+
+const getNotificationData = (
+  type: 'success' | 'error' | 'info',
+  message: string,
+): { icon: JSX.Element; notificationMessage: JSX.Element } => {
+  const successMessage = <H3Text style={{ color: 'rgba(236, 38, 245, 1)' }} text={message} />;
+  const infoMessage = <H3Text style={{ color: 'rgba(1, 86, 255, 1)' }} text={message} />;
+  const errorMessage = <H3Text style={{ color: 'rgba(238, 60, 153, 1)' }} text={message} />;
+  const successIcon = (
+    <SuccessIcon
+      style={{ height: '30px', width: '30px', marginTop: '3px', stroke: 'rgba(236, 38, 245, 1)' }}
+    />
+  );
+  const infoIcon = (
+    <SuccessIcon
+      style={{ height: '30px', width: '30px', marginTop: '3px', stroke: 'rgba(1, 86, 255, 1)' }}
+    />
+  );
+  const errorIcon = <ErrorIcon style={{ height: '30px', width: '30px', marginTop: '3px' }} />;
+
+  switch (type) {
+    case 'success':
+      return { icon: successIcon, notificationMessage: successMessage };
+    case 'info':
+      return { icon: infoIcon, notificationMessage: infoMessage };
+    case 'error':
+      return { icon: errorIcon, notificationMessage: errorMessage };
+    default:
+      throw new Error(`Unrecognized notification type: ${type}`);
+      break;
+  }
+};
 
 export function notify({
   message,
@@ -14,44 +47,58 @@ export function notify({
   message: string;
   description?: string | JSX.Element;
   txid?: string;
-  type: 'error' | 'success';
+  type: 'error' | 'success' | 'info';
   placement?: string;
 }) {
-  const successMessage = <H3Text style={{ color: 'rgba(236, 38, 245, 1)' }} text={message} />;
-  const errorMessage = <H3Text style={{ color: 'rgba(238, 60, 153, 1)' }} text={message} />;
-  const successIcon = (
-    <img
-      alt={'success-icon'}
-      src={SuccessIcon}
-      style={{ height: '30px', width: '30px', marginTop: '3px' }}
-    />
-  );
-  const errorIcon = (
-    <img
-      alt={'error-icon'}
-      src={ErrorIcon}
-      style={{ height: '30px', width: '30px', marginTop: '3px' }}
-    />
-  );
+  const { notificationMessage, icon } = getNotificationData(type, message);
 
   notification[type]({
     placement: 'topRight',
-    message: type === 'success' ? successMessage : errorMessage,
+    message: notificationMessage,
     description:
       typeof description !== 'string' ? (
         description
       ) : (
         <BodyText style={{ textAlign: 'left' }} text={description} />
       ),
-    icon: type === 'success' ? successIcon : errorIcon,
+    icon,
     closeIcon: <CloseOutlined style={{ fontSize: '24px', color: '#FFFFFF' }} />,
     style: {
-      width: '495px',
-      height: '168px',
       padding: '25px',
       border: '1px solid #0156FF',
       borderRadius: '8px',
       backgroundColor: '#0A0C0E',
     },
   });
+}
+
+const useStyles = makeStyles(() => ({
+  text: {
+    color: '#fff',
+    marginRight: '5px',
+  },
+  link: {
+    'color': '#fff',
+    '&:hover': {
+      underline: '1px solid #fff',
+    },
+  },
+}));
+
+export function SolscanDescription({ txid }: { txid: string }) {
+  const styles = useStyles();
+
+  return (
+    <div>
+      <span className={styles.text}>View on</span>
+      <a
+        className={styles.link}
+        href={`https://solscan.io/tx/${txid}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Solscan
+      </a>
+    </div>
+  );
 }
